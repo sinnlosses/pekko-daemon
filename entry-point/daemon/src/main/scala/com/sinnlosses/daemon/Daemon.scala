1e97155ec41d6c30270f3ee79aa703a71dfc5534
fuji <fujiyoshiyuki@gmail.com>
2026-01-20 22:50:26 +0900

    シンプルに

--- entry-point/daemon/src/main/scala/com/sinnlosses/daemon/Daemon.scala
+++ entry-point/daemon/src/main/scala/com/sinnlosses/daemon/Daemon.scala
@@ -1,29 +1,25 @@
 package com.sinnlosses.daemon
 
-import com.twitter.util.Await
-import com.twitter.util.Future
-import com.twitter.util.Promise
-import org.apache.pekko.Done
-import org.apache.pekko.NotUsed
+import com.twitter.util.{Await, Future => TwitterFuture, Promise}
+import org.apache.pekko.{Done, NotUsed}
 import org.apache.pekko.actor.ActorSystem
 import org.apache.pekko.stream._
 import org.apache.pekko.stream.scaladsl._
 
-import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.{Future => ScalaFuture}
 import scala.concurrent.duration._
-import scala.util.Failure
-import scala.util.Success
+import scala.util.{Failure, Success}
 
 class Daemon extends com.twitter.inject.app.App {
 
   override def run(): Unit = {
-    implicit val system: ActorSystem = ActorSystem("QuickStart")
+    implicit val system: ActorSystem = ActorSystem("Daemon")
     val future = handleSubCommand()
 
     Await.result(future)
   }
 
-  private def handleSubCommand()(implicit actorSystem: ActorSystem): Future[Unit] = {
+  private def handleSubCommand()(implicit actorSystem: ActorSystem): TwitterFuture[Unit] = {
     val graph = makeGraph(subCommand())
     runGraph(graph)
   }
@@ -55,15 +51,12 @@ class Daemon extends com.twitter.inject.app.App {
           val concat = builder.add(Concat[NotUsed]())
           val (queue, source) = Source.queue[NotUsed](1, OverflowStrategy.fail).preMaterialize()
 
-          val ticker = builder.add(
-            Flow[NotUsed]
-              .throttle(
-                elements = 1,
-                per = 5.seconds,
-                maximumBurst = 1,
-                mode = ThrottleMode.Shaping
-              )
-          )
+          val ticker = builder.add(Flow[NotUsed].throttle(
+            elements = 1,
+            per = 5.seconds,
+            maximumBurst = 1,
+            mode = ThrottleMode.Shaping
+          ))
 
           val main = builder.add(Flow[NotUsed].flatMapConcat { _ =>
             mainAsSubSource(mainFlow)
@@ -84,16 +77,17 @@ class Daemon extends com.twitter.inject.app.App {
   }
 
   private def mainAsSubSource[T, E](mainFlow: Flow[NotUsed, Either[E, T], NotUsed])
-      : Graph[SourceShape[Either[E, T]], concurrent.Future[Done]] = {
+      : Graph[SourceShape[Either[E, T]], ScalaFuture[Done]] = {
     // サブソース自身の完了を materialized value として返す
     Source
       .single(NotUsed)
       .via(mainFlow)
+      .completionTimeout(5.seconds)
       .watchTermination()(Keep.right)
   }
 
   private def makeGraph[E <: Throwable, T](source: Source[Either[E, T], UniqueKillSwitch])
-      : RunnableGraph[(UniqueKillSwitch, concurrent.Future[Done])] = {
+      : RunnableGraph[(UniqueKillSwitch, ScalaFuture[Done])] = {
     val errorSink = Sink.foreach[Either[E, T]] {
       case Right(_) => // ignore
       case Left(e)  => logger.error(e.getMessage)
@@ -102,9 +96,9 @@ class Daemon extends com.twitter.inject.app.App {
     source.toMat(errorSink)(Keep.both)
   }
 
-  private def runGraph(graph: RunnableGraph[(UniqueKillSwitch, concurrent.Future[Done])])(implicit
+  private def runGraph(graph: RunnableGraph[(UniqueKillSwitch, ScalaFuture[Done])])(implicit
       actorSystem: ActorSystem
-  ): Future[Unit] = {
+  ): TwitterFuture[Unit] = {
     import actorSystem.dispatcher
 
     info("デーモンプロセス開始")
@@ -118,70 +112,6 @@ class Daemon extends com.twitter.inject.app.App {
 
     promise.setInterruptHandler(_ => killSwitch.shutdown())
     promise
-  }
-
-  private def process001(): Unit = {
-    implicit val system: ActorSystem = ActorSystem("QuickStart")
-    implicit val ec: ExecutionContextExecutor = system.dispatcher
-
-    // Source を queue にし、後から要素を追加
-    val source = Source.queue[Int](bufferSize = 16, OverflowStrategy.backpressure)
-    val sink = Sink.fold[Int, Int](0) { (acc, x) =>
-      val s = acc + x
-      println(s"[sink] received=$x, sum=$s")
-      s
-    }
-
-    val g: RunnableGraph[(SourceQueueWithComplete[Int], concurrent.Future[Int])] =
-      RunnableGraph.fromGraph(
-        GraphDSL.createGraph(source, sink)((q, f) => (q, f)) {
-          implicit builder => (in, out) =>
-            import GraphDSL.Implicits._
-
-            val f1 = Flow[Int].map(_ + 10)
-
-        // format: off
-        // in ~> f1 ~> bcast
-        //             bcast ~> f2 ~> merge
-        //             bcast ~> f4 ~> merge
-        //                            merge ~> f3 ~> out
-          in ~> f1 ~> out
-        // format: on
-
-            ClosedShape
-        }
-      )
-
-    val (queue, resultF) = g.run()
-
-    // 3秒に1回、1を投入
-    val tick = system.scheduler.scheduleAtFixedRate(0.seconds, 3.seconds)(() => {
-      queue.offer(1); ()
-    })
-
-    // デモ用に20秒後に完了して合計を出力
-    system.scheduler.scheduleOnce(20.seconds) {
-      tick.cancel()
-      queue.complete()
-    }
-
-    resultF.onComplete { _ =>
-      system.terminate()
-    }
-  }
-
-  private def process002(): Unit = {
-    implicit val system: ActorSystem = ActorSystem("QuickStart")
-    implicit val ec: ExecutionContextExecutor = system.dispatcher
-
-    val (switch: UniqueKillSwitch, done) = Source.tick(0.seconds, 1.second, "tick")
-      .viaMat(KillSwitches.single)(Keep.right)
-      .toMat(Sink.foreach(println))(Keep.both)
-      .run()
-
-    system.scheduler.scheduleOnce(5.seconds) {
-      switch.shutdown() // 正常完了にする（abort(ex) で失敗完了も可）
-    }
   }
 }
 
